fcc814c47e4f6fc9b1936588d3233774d194f651
aishaapat <aishapatel0703@gmail.com>
2024-11-30 18:30:32 -0500

    just importing what we need for reset, will finish ui by sunday

--- sample/src/main/java/org/com/screens/security/ResetPasswordScreen.java
+++ sample/src/main/java/org/com/screens/security/ResetPasswordScreen.java
@@ -1,8 +1,29 @@
 package org.com.screens.security;
+/*
 
+import org.com.bases.Screen;
+import org.com.components.buttons.EnterBtn;
+import org.com.components.InputField;
+import org.com.components.buttons.MainMenuBtn;
+import org.com.constants.Sizes;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import org.com.db.UserDatabase;
+
+import java.sql.SQLException;
+import java.util.Map;
+import org.com.components.panes.SecurityQuestionPane;
+
+import java.sql.Connection;
+*/
 
 // Users will reset their password in this screen by first entering their username
-public class ResetPasswordScreen {
+public class ResetPasswordScreen
+{
     
 }
